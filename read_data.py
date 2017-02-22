d011ba3a83ddf8dd4e083acf4dfda013f63de46e
Dhruv Mehra <dmehra6@gatech.edu>
2017-02-22 12:06:35 -0500

    Read all the filenames in chronological order

--- read_data.py
+++ read_data.py
@@ -1,34 +1,53 @@
 import tensorflow as tf
 from tensorflow import TensorArray as ta
 
-features = tf.placeholder(tf.int32, shape=[5], name='datapoints')
-timestamp = tf.placeholder(tf.int32, name='timestamp')
-total = tf.reduce_sum(features, name='total')
-printerop = tf.Print(total, [timestamp, features, total], name='printer')
+from os import walk, getcwd, listdir
+from os.path import basename
 
-def create_file_reader(filename_queue):
-    reader = tf.TextLineReader(skip_header_lines=0)
-    _, row = reader.read(filename_queue)
-    defaults = [[0], [0], [0], [0], [0], [0]]
-    timestamp, point_1, point_2, point_3, point_4, point_5 = tf.decode_csv(row, record_defaults=defaults)
-    features = tf.stack([point_1, point_2, point_3, point_4, point_5])
-    return timestamp, features
+# features = tf.placeholder(tf.int32, shape=[5], name='datapoints')
+# timestamp = tf.placeholder(tf.int32, name='timestamp')
+# total = tf.reduce_sum(features, name='total')
+# printerop = tf.Print(total, [timestamp, features, total], name='printer')
 
-filenames = ['data/hand1.csv', 'data/hand2.csv']
-filename_queue = tf.train.string_input_producer(filenames)
-timestamp, features = create_file_reader(filename_queue)
+# def create_file_reader(filename_queue):
+#     reader = tf.TextLineReader(skip_header_lines=0)
+#     _, row = reader.read(filename_queue)
+#     defaults = [[0], [0], [0], [0], [0], [0]]
+#     timestamp, point_1, point_2, point_3, point_4, point_5 = tf.decode_csv(row, record_defaults=defaults)
+#     features = tf.stack([point_1, point_2, point_3, point_4, point_5])
+#     return timestamp, features
 
-with tf.Session() as sess:
-    tf.global_variables_initializer().run()
-    coord = tf.train.Coordinator()
-    threads = tf.train.start_queue_runners(coord=coord)
+# filenames = ['data/hand1.csv', 'data/hand2.csv']
+# filename_queue = tf.train.string_input_producer(filenames)
+# timestamp, features = create_file_reader(filename_queue)
 
-    for i in range(10):
-        time, points = sess.run([timestamp, features])
-        print(points)
-        # print(time)
+# with tf.Session() as sess:
+#     tf.global_variables_initializer().run()
+#     coord = tf.train.Coordinator()
+#     threads = tf.train.start_queue_runners(coord=coord)
 
-coord.request_stop()
-coord.join(threads)
+#     for i in range(10):
+#         time, points = sess.run([timestamp, features])
+#         print(points)
+#         # print(time)
+
+# coord.request_stop()
+# coord.join(threads)
 
 # Progression -> read_data -> create_or_find_bottleneck -> train_last_layer
+
+def get_image_names(data_dir):
+    data_files = ['hand1']
+
+    for file in data_files:
+        path = "{}/images/{}".format(data_dir, file)
+
+        samples = listdir(path)
+        samples = ["{}/{}".format(path, x) for x in sorted(samples)]
+        for i in range(50):
+            print(samples[i])
+
+if __name__ == '__main__':
+    get_image_names(getcwd() + '/data')
+    
+        
